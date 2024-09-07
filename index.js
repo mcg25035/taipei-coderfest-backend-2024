ec701d4dce0543b3bd9d01c90dd42a38615aa8e3
南極雄 <70533278+mcg25035@users.noreply.github.com>
2024-09-08 00:41:21 +0800

    prevent 413

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 
 const app = express();
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(cors({
     origin: '*',
     methods: 'GET,POST,PUT,DELETE',
